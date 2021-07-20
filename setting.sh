9b2cac341777a5aef4dc3cb2339c87bfa9bb5aa4
Young Rok Jang <jyrok3357@gmail.com>
2021-07-20 11:23:11 +0900

    Create setting.sh

---
+++ setting.sh
@@ -0,0 +1,7 @@
+apt-get update
+apt-get install net-tools vim openssh-server tmux -y
+sed -i -e '/PermitRootLogin/d' -e '$ a PermitRootLogin yes' /etc/ssh/sshd_config
+passwd root
+service ssh restart
+pip install jupyterlab
+nohup jupyter-lab --port 9010 --ip 0.0.0.0 --allow-root > jupyter.out &
