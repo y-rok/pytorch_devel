a76ddd2bd6d7e9841184ea452bc21928d3fc9041
Young Rok Jang <jyrok3357@gmail.com>
2021-07-20 19:24:50 +0900

    Update setting.sh

--- setting.sh
+++ setting.sh
@@ -1,7 +1,7 @@
 apt-get update
 apt-get install net-tools vim openssh-server tmux -y
 sed -i -e '/PermitRootLogin/d' /etc/ssh/sshd_config
-sed -i -e '$ a PermitRootLogin yes' 
+sed -i -e '$ a PermitRootLogin yes' /etc/ssh/sshd_config
 passwd root
 service ssh restart
 pip install jupyterlab
